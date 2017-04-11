8197221cd12cccfc1666a6fe53cf8251f82c26a2
ralphwillgoss <ralph.willgoss@gmail.com>
2017-04-11 22:26:56 +0000

    Add async function to fetch url's

--- index.js
+++ index.js
@@ -4,45 +4,38 @@ const program = require('commander');
 const csv = require('csv');
 const fs = require('fs');
 const inquirer = require('inquirer'); 
+const async = require('async');
+const chalk = require('chalk');
+const fetch = require('node-fetch');
 
 program
   .version('0.0.1')
   .option('-l, --list [list]', 'List of customers in CSV')
   .parse(process.argv)
 
-let questions = [
-  {
-    type : "input",
-    name : "sender.email",
-    message : "Sender's email address - "
-  },
-  {
-    type : "input",
-    name : "sender.name",
-    message : "Sender's name - "
-  },
-  {
-    type : "input",
-    name : "subject",
-    message : "Subject - "
-  }
-];
-let contactList = [];
+let domains = [];
 let parse = csv.parse;
 let stream = fs.createReadStream(program.list)
-    .pipe(parse({ delimiter : "," }));
+               .pipe(parse({ delimiter : "," }));
 
 stream
   .on("error", function (err) {
-    return console.error(err.message);
+    return console.error(err.response);
   })
   .on("data", function (data) {
-    let name = data[0] + " " + data[1];
-    let email = data[2];
-    contactList.push({ name : name, email : email });
+    let name = data[0];
+    let url = data[1];
+    domains.push({ name : name, url : url });
   })
   .on("end", function () {
-    inquirer.prompt(questions).then(function (answers) {
-      console.log(answers);
+    async.each(domains, function (domain, fn) {
+        let o = fetch(domain.url)
+                    .then(resp => console.log(domain.url+ ": " +chalk.green(resp.status)))
+                    .catch(err => console.log(chalk.green(err.message)))
+      }, function (err) {
+        if (err) {
+          return console.error(chalk.red(err.message));
+        }
+        console.log(chalk.green('Success'));
     });
-  });
+});
